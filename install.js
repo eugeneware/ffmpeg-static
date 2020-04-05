e438ca5011faf4ececeaf0dfa8edd98cbaffd88a
Jannis R <mail@jannisr.de>
2020-04-06 00:58:34 +0200

    install script: cache ffmpeg binaries
    see #37

--- install.js
+++ install.js
@@ -2,8 +2,12 @@
 
 var fs = require("fs");
 var os = require("os");
+const {encode: encodeQuery} = require('querystring')
+const {strictEqual} = require('assert')
+const envPaths = require('env-paths')
+const FileCache = require('@derhuerst/http-basic/lib/FileCache').default
 var ProgressBar = require("progress");
-var request = require('http-basic')
+var request = require('@derhuerst/http-basic')
 var ffmpegPath = require(".");
 var pkg = require("./package");
 
@@ -20,6 +24,30 @@ if (!ffmpegPath) {
   exitOnError('ffmpeg-static install failed: No binary found for architecture')
 }
 
+// https://advancedweb.hu/how-s3-signed-urls-work/
+const normalizeS3Url = (url) => {
+  url = new URL(url)
+  if (url.hostname.slice(-17) !== '.s3.amazonaws.com') return url.href
+  const query = Array.from(url.searchParams.entries())
+  .filter(([key]) => key.slice(0, 6).toLowerCase() !== 'x-amz-')
+  .reduce((query, [key, val]) => ({...query, [key]: val}), {})
+  url.search = encodeQuery(query)
+  return url.href
+}
+strictEqual(
+  normalizeS3Url('https://example.org/foo?bar'),
+  'https://example.org/foo?bar'
+)
+strictEqual(
+  normalizeS3Url('https://github-production-release-asset-2e65be.s3.amazonaws.com/29458513/26341680-4231-11ea-8e36-ae454621d74a?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20200405%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20200405T225358Z&X-Amz-Expires=300&X-Amz-Signature=d6415097af04cf62ea9b69d3c1a421278e96bcb069afa48cf021ec3b6941bae4&X-Amz-SignedHeaders=host&actor_id=0&response-content-disposition=attachment%3B%20filename%3Ddarwin-x64&response-content-type=application%2Foctet-stream'),
+  'https://github-production-release-asset-2e65be.s3.amazonaws.com/29458513/26341680-4231-11ea-8e36-ae454621d74a?actor_id=0&response-content-disposition=attachment%3B%20filename%3Ddarwin-x64&response-content-type=application%2Foctet-stream'
+)
+
+const cache = new FileCache(envPaths(pkg.name).cache)
+cache.getCacheKey = (url) => {
+  return FileCache.prototype.getCacheKey(normalizeS3Url(url))
+}
+
 const noop = () => {}
 function downloadFile(url, destinationPath, progressCallback = noop) {
   let fulfill, reject;
@@ -34,6 +62,7 @@ function downloadFile(url, destinationPath, progressCallback = noop) {
     followRedirects: true,
     maxRedirects: 3,
     gzip: true,
+    cache,
     timeout: 30 * 1000, // 30s
     retry: true,
   }, (err, response) => {
@@ -52,9 +81,9 @@ function downloadFile(url, destinationPath, progressCallback = noop) {
     file.on("error", error => reject(error));
     response.body.pipe(file)
 
-    totalBytes = parseInt(response.headers["content-length"], 10);
-
-    if (progressCallback) {
+    if (!response.fromCache && progressCallback) {
+      const cLength = response.headers["content-length"]
+      totalBytes = cLength ? parseInt(cLength, 10) : null
       response.body.on('data', (chunk) => {
         progressCallback(chunk.length, totalBytes);
       });
@@ -66,6 +95,7 @@ function downloadFile(url, destinationPath, progressCallback = noop) {
 
 let progressBar = null;
 function onProgress(deltaBytes, totalBytes) {
+  if (totalBytes === null) return;
   if (!progressBar) {
     progressBar = new ProgressBar(`Downloading ffmpeg [:bar] :percent :etas `, {
       complete: "|",
