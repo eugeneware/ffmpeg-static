62fa91e7a609ef8635d9aedba7cf574d86fd93a5
Romick2005 <17779660+Romick2005@users.noreply.github.com>
2021-07-05 10:57:41 +0300

    Update index.js
    Why Object.assign and Object.create needed here? Am I missing something?

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ if (process.env.FFMPEG_BIN) {
   var os = require('os')
   var path = require('path')
 
-  var binaries = Object.assign(Object.create(null), {
+  var binaries = {
     darwin: ['x64', 'arm64'],
     freebsd: ['x64'],
     linux: ['x64', 'ia32', 'arm64', 'arm'],
     win32: ['x64', 'ia32']
-  })
+  }
 
   var platform = process.env.npm_config_platform || os.platform()
   var arch = process.env.npm_config_arch || os.arch()
