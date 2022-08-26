6931eb71fd555434d53580e8bfeb1b33b8055bd7
Emil Sivervik <emil@sivervik.com>
2022-08-26 14:04:06 +0200

    index.js: pull binary path env var from package.json

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 'use strict'
 
-if (process.env.FFMPEG_BIN) {
-  module.exports = process.env.FFMPEG_BIN
+const pkg = require('./package.json')
+
+const {
+  'binary-path-env-var': BINARY_PATH_ENV_VAR,
+  'executable-base-name': executableBaseName,
+} = pkg[pkg.name]
+if ('string' !== typeof BINARY_PATH_ENV_VAR) {
+  throw new Error(`package.json: invalid/missing ${pkg.name}.binary-path-env-var entry`)
+}
+if ('string' !== typeof executableBaseName) {
+  throw new Error(`package.json: invalid/missing ${pkg.name}.executable-base-name entry`)
+}
+
+if (process.env[BINARY_PATH_ENV_VAR]) {
+  module.exports = process.env[BINARY_PATH_ENV_VAR]
 } else {
   var os = require('os')
   var path = require('path')
-  const pkg = require('./package.json')
-  const {
-    'executable-base-name': executableBaseName,
-  } = pkg[pkg.name]
-  if ('string' !== typeof executableBaseName) {
-    throw new Error(`package.json: invalid/missing ${pkg.name}.executable-base-name entry`)
-  }
 
   var binaries = Object.assign(Object.create(null), {
     darwin: ['x64', 'arm64'],
@@ -23,14 +29,14 @@ if (process.env.FFMPEG_BIN) {
   var platform = process.env.npm_config_platform || os.platform()
   var arch = process.env.npm_config_arch || os.arch()
 
-  var ffmpegPath = path.join(
+  let binaryPath = path.join(
     __dirname,
     executableBaseName + (platform === 'win32' ? '.exe' : ''),
   )
 
   if (!binaries[platform] || binaries[platform].indexOf(arch) === -1) {
-    ffmpegPath = null
+    binaryPath = null
   }
 
-  module.exports = ffmpegPath
+  module.exports = binaryPath
 }
